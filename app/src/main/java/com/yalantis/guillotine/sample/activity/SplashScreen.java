1c014830cf1e0382fc76251c20b1af50be2bbffc
kanishk sharma <kanishksharma3012@gmail.com>
2016-03-30 13:20:28 +0530

    Splash screen adeed

---
+++ app/src/main/java/com/yalantis/guillotine/sample/activity/SplashScreen.java
@@ -0,0 +1,68 @@
+package com.yalantis.guillotine.sample.activity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.yalantis.guillotine.sample.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by my hp on 3/30/2016.
+ */
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        requestjson();
+    }
+
+    private void requestjson() {
+        String url="http://zealbo.hackncs.com/events/0";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        Log.v("MyApp", response);
+                        SharedPreferences s=getSharedPreferences("events",0);
+                        s.edit().putString("eventsjson",response).apply();
+                        Intent in=new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(in);
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //You can handle error here if you want
+                        Log.v("MyApp",error.toString());
+                    }
+                });
+
+        //Adding the string request to the queue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+        int socketTimeout = 20000;//20 seconds - change to what you want
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(policy);
+
+    }
+}
