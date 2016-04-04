b93fe779e497bd0944b4790c9e19f063bbbf4db6
kanishk sharma <kanishksharma3012@gmail.com>
2016-04-05 00:10:25 +0530

    Registration and sponsors gui improved and intro deleted

--- app/src/main/java/com/yalantis/guillotine/sample/activity/Sponsors.java
+++ app/src/main/java/com/yalantis/guillotine/sample/activity/Sponsors.java
@@ -3,6 +3,7 @@ package com.yalantis.guillotine.sample.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.yalantis.guillotine.sample.R;
 
@@ -20,5 +21,22 @@ public class Sponsors extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                // NavUtils.navigateUpFromSameTask(this);
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
