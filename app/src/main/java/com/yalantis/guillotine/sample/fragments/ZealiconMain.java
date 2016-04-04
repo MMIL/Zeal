6157ac14c16675af85e9915146067bfd1f107e07
kanishk sharma <kanishksharma3012@gmail.com>
2016-04-05 02:16:44 +0530

    Registration module added and unneccesary files deleted

--- app/src/main/java/com/yalantis/guillotine/sample/fragments/ZealiconMain.java
+++ app/src/main/java/com/yalantis/guillotine/sample/fragments/ZealiconMain.java
@@ -9,10 +9,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.yalantis.guillotine.sample.R;
 import com.yalantis.guillotine.sample.activity.Register;
+import com.yalantis.guillotine.sample.activity.RegisterTry;
 
 /**
  * Created by my hp on 3/30/2016.
@@ -31,13 +33,24 @@ public class ZealiconMain extends Fragment {
 v.findViewById(R.id.imageview_register).setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View v) {
-        Intent intent=new Intent(getContext(), Register.class);
+        Intent intent = new Intent(getContext(), Register.class);
         startActivity(intent);
     }
 });
         TextView tv_extravaganza=(TextView)v.findViewById(R.id.tv_extravaganza);
         Typeface custom_font_extravaganza = Typeface.createFromAsset(getActivity().getAssets(), "huggable.ttf");
         tv_extravaganza.setTypeface(custom_font_extravaganza);
+        LinearLayout ll=(LinearLayout)v.findViewById(R.id.zealid_linear);
+        String s=getActivity().getSharedPreferences("zealiconid",0).getString("zealid","hp");
+        if(s.equals("hp")){
+            ll.setVisibility(View.INVISIBLE);
+        }
+        else {
+            TextView tvzeal=(TextView)v.findViewById(R.id.main_zealid);
+            tvzeal.setText("Your Zealicon Id is "+s);
+            tvzeal.setTypeface(custom_font_extravaganza);
+            ll.setVisibility(View.VISIBLE);
+        }
 
         return v;
     }
